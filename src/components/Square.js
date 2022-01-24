c5f374bc9920ee4fd91fd1893bde96d587d12a58
Praduman kumar <64727650+Praduman2k19@users.noreply.github.com>
2022-01-24 19:49:39 +0530

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Square = ({value}) => {   
+    console.log(value);
+    return (
+        <button type='button'>{value}</button>          
+    )
+}
+
+
+// const Square = props => {   
+//     console.log(props);
+//     return (
+//         <button type='button'>{props.value}</button>          
+//     )
+// }
+
+export default Square
